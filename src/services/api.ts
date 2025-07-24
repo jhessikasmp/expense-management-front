73a30c70833e268beb7c7fb57b39ef00d0522e61
Seu Nome <jhessika.smp@gmail.com>
2025-07-24 22:46:17 +0200

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { User, Expense, Investment, TravelFund } from '../types';
 
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://expense-management-back.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
