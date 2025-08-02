48e5219174ac1f21dc5e58a0e6316de908eb5a0a
Seu Nome <jhessika.smp@gmail.com>
2025-08-02 18:29:36 +0200

    modificacoes 02/08

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import { Expense, User } from '../types';
 import { expenseService, fundService } from '../services/api';
 import { useTheme } from '../hooks/useTheme';
